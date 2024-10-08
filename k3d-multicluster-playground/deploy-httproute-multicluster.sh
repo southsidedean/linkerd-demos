62007b99d637705e78eaf8fb3237a560e7efcf17
Tom Dean <tom@dean33.com>
2024-10-07 23:35:39 -0500

    Work on multicluster script for httproute demo.

--- k3d-multicluster-playground/deploy-httproute-multicluster.sh
+++ k3d-multicluster-playground/deploy-httproute-multicluster.sh
@@ -73,8 +73,8 @@ kubectl apply -f policy.yaml --context k3d-$CLUSTER_B_NAME
 #KC1=`linkerd --context=k3d-cluster-a3 multicluster link --cluster-name cluster-a3 | grep kubeconfig: | uniq | awk {'print $2'}` ; KC2=`echo $KC1 | base64 -d | sed 's/0\.0\.0\.0/kubernetes/g' | base64` ; awk -f mc.awk "$KC1" "$KC2" multicluster-link-orig-a3.yaml >> multicluster-link.yaml
 
 linkerd --context=k3d-cluster-a1 multicluster link --cluster-name cluster-a1 >> multicluster-link.yaml
-linkerd --context=k3d-cluster-a2 multicluster link --cluster-name cluster-a1 >> multicluster-link.yaml
-linkerd --context=k3d-cluster-a3 multicluster link --cluster-name cluster-a1 >> multicluster-link.yaml
+linkerd --context=k3d-cluster-a2 multicluster link --cluster-name cluster-a2 >> multicluster-link.yaml
+linkerd --context=k3d-cluster-a3 multicluster link --cluster-name cluster-a3 >> multicluster-link.yaml
 
 kubectl apply -f multicluster-link.yaml --context k3d-$CLUSTER_B_NAME
 kubectl get links -A --context=k3d-$CLUSTER_B_NAME
